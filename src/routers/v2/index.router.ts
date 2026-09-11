31fa0adda1e629370742b2b3e9537a503941462c
Hiranmay Mushahary <hiranmaymushahary127@gmail.com>
2026-09-11 14:10:49 +0530

    Added api versioning and nodemon

---
+++ src/routers/v2/index.router.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+
+const v2Router = express.Router();
+
+
+
+
+export default v2Router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { serverConfig} from "./config"
-import pingRouter from "./routers/ping.router";
+import v1Router from "./routers/v1/index.router";
+import v2Router from "./routers/v2/index.router";
 
 const app = express();
 
@@ -9,7 +10,10 @@ const app = express();
  * Registering all the routers and their corresponding routes with out app server object.
  */
 
-app.use(pingRouter);
+app.use("/api/v1",v1Router);
+app.use("/api/v2",v2Router);
+
+
 
 
 
